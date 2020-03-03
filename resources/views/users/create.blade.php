316e3f201f56a593415b71b8efa9933d6b04752c
NgoTruong2911 <kizth123@gmail.com>
2020-03-03 16:48:33 +0700

    [UPDATE]update view

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,11 +1,13 @@
 @extends('master.layout')
 @section('content')
 <div class="content-wrapper">
+<div class="container">
+
     <!-- Content Header (Page header) -->
     <form action="{{ route('users.store') }}" enctype="multipart/form-data" method="POST">
         @csrf
         <div class="form-group">
-            <label for="name">Name</label>
+            <label for="name">Name<a class="text-danger">*</a></label>
             <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}"
                 placeholder="Enter name">
             @error('name')
@@ -13,7 +15,7 @@
             @enderror
         </div>
         <div class="form-group">
-            <label for="dateofbirth">Birthday</label>
+            <label for="dateofbirth">Birthday<a class="text-danger">*</a></label>
             <input type="date" class="form-control birthday" value="{{ old('birthday') }}" id="dateofbirth"
                 name="birthday">
             @error('birthday')
@@ -21,7 +23,7 @@
             @enderror
         </div>
         <div class="form-group">
-            <label for="phone_number">Phone number</label>
+            <label for="phone_number">Phone number<a class="text-danger">*</a></label>
             <input type="text" class="form-control" id="phone_number" value="{{ old('phone_number') }}"
                 name="phone_number">
             @error('phone_number')
@@ -29,14 +31,14 @@
             @enderror
         </div>
         <div class="form-group">
-            <label for="email">Email</label>
+            <label for="email">Email<a class="text-danger">*</a></label>
             <input type="email" class="form-control" id="email" value="{{ old('email') }}" name="email">
             @error('email')
             <p class="text-danger">{{ $message }}</p>
             @enderror
         </div>
         <div class="form-group">
-            <label for="faculty_id">Faculty name</label>
+            <label for="faculty_id">Faculty name<a class="text-danger">*</a></label>
             <select class="form-control" name="faculty_id" id="faculty_id">
                 @foreach($faculties as $faculty)
                 <option value="{{ $faculty->id }}" }}>
@@ -49,7 +51,7 @@
             @enderror
         </div>
         <div class="form-group">
-            <label for="gender">Gender : </label>
+            <label for="gender">Gender : <a class="text-danger">*</a></label>
             <input type="radio" name="gender" value="1">Female
             <input type="radio" name="gender" value="2">Male
             @error('gender')
@@ -57,13 +59,13 @@
             @enderror
         </div>
         <div class="form-group">
-            <label for="avatar">Avatar</label>
+            <label for="avatar">Avatar<a class="text-danger">*</a></label>
             <input type="file" id="avatar" name="avatar">
         </div>
         @error('avatar')
         <p class="text-danger">{{ $message }}</p>
         @enderror
-        <div class="form-group">
+        {{-- <div class="form-group">
             <a class="btn btn-primary  mb-3 mt-3" data-toggle="collapse" href="#collapseExample" role="button"
                 aria-expanded="false" aria-controls="collapseExample">
                 Role
@@ -79,9 +81,18 @@
                     @endforeach
                 </div>
             </div>
+        </div> --}}
+        <div class="form-group">
+            <label for="roles">Roles :<a class="text-danger">*</a> </label>
+            @foreach($roles as $role =>$val)
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" name="roles[]" id="inlineRadio1" value="{{$role}}">
+                <label class="form-check-label" for="inlineRadio">{{$val}}</label>
+              </div>
+            @endforeach
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password">Password<a class="text-danger">*</a></label>
             <input type="password" name="password" class="form-control" id="password" value="{{old('password')}}"
                 placeholder="Enter Password">
         </div>
@@ -89,7 +100,7 @@
         <div class="text-danger">{{ $message }}</div>
         @enderror
         <div class="form-group">
-            <label for="password">Confirm Password</label>
+            <label for="password">Confirm Password<a class="text-danger">( * )</a></label>
             <input type="password" name="password_confirmation" class="form-control" id="password_confirmation"
                 value="{{old('password')}}" placeholder="Enter Password">
         </div>
@@ -99,6 +110,7 @@
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
     <!-- /.content -->
+</div>
 </div>
 @endsection
 <script>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,182 +1,20 @@
 @extends('master.layout')
 <div class="container">
     @section('content')
-
-    <div>
-    </div>
     <div class="content-wrapper">
-
         <div class="container">
             @if(session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
             </div>
             @endif
-            <form action="{{ route('users.update',[$user->id]) }}" enctype="multipart/form-data" method="POST">
-                @csrf
-                @method('PUT')
-                <input type="hidden" name="id" value="{{$user->id}}">
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control @error('name') is-invalid @enderror"
-                        value="{{ old('name',$user->name) }}" id="name" name="name" placeholder="Enter name">
-                    @error('name')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="dateofbirth">Birthday</label>
-                    <input type="date" class="form-control birthday" value="{{ old('birthday',$user->birthday) }}"
-                        id="dateofbirth" name="birthday">
-                    @error('birthday')
-                    <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="phone_number">Phone number</label>
-                    <input type="text" class="form-control" value="{{ old('phone_number',$user->phone_number) }}"
-                        id="phone_number" name="phone_number">
-                    @error('phone_number')
-                    <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control @error('email') is-invalid @enderror"
-                        value="{{ old('email',$user->email) }}" id="email" name="email">
-                    @error('email')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="faculty_id">Faculty name</label>
-                    <select class="form-control" name="faculty_id" id="faculty_id">
-                        @foreach($faculties as $faculty)
-                        <option value="{{ $faculty->id }}" {{ $user->faculty_id == $faculty->id ? 'selected' : '' }}>
-                            {{ $faculty->name }}
-                        </option>
-                        @endforeach
-                    </select>
-                    @error('faculty_id')
-                    <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                @if($user->gender==null)
-                <div class="form-group">
-                    <label for="gender">Gender : </label>
-                    <input type="radio" name="gender" value="1">Female
-                    <input type="radio" name="gender" value="2">Male
-                    @error('gender')
-                    <p class="text-danger">{{$message}}</p>
-                    @enderror
-                </div>
-                @else
-                <div class="form-group">
-                    <label for="gender">Gender : </label>
-                    @if($user->gender==1)
-                    <label class="checkbox-inline"><input type="radio" name="gender" checked value="1">Female</label>
-                    <label class="checkbox-inline"><input type="radio" name="gender" value="2">Male</label>
-                    @endif
-                    @if($user->gender==2)
-                    <label class="checkbox-inline"><input type="radio" name="gender" value="1">Female</label>
-                    <label class="checkbox-inline"><input type="radio" name="gender" checked value="2">Male</label>
-                    @endif
-                    @error('gender')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                @endif
-                <div class="form-group">
-                    <label for="avatar">Avatar : </label>
-                    <input type="file" id="avatar" class=" form-control @error('avatar') is-invalid @enderror" name="avatar">
-                    @if(!empty($user->avatar))
-                    <img src="{{asset ( $user->avatar ) }}" class="rounded-lg ml-2" alt="avatar" width="100px" height="100px">
-                    @endif
-                </div>
-                @error('avatar')
-                <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-        @if(!empty( $user->id ))
-        @if((!empty ( $user->password ) && Auth::user()->hasRole('user')) || (
-        Auth::user()->hasRole('admin') &&
-        Auth::user()->id == $user->id && !empty ( Auth::user()->password )))
-        <div class="form-group">
-            <label for="current_password">Old Password</label>
-            <input type="password" name="current_password" class="form-control" id="current_password"
-                value="{{old('current_password')}}" placeholder="Enter your Password">
-        </div>
-        @endif
-        @error('current_password')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-        <div class="form-group">
-            <label for="password">New Password</label>
-            <input type="password" name="password" class="form-control" id="password_confirmation"
-                value="{{old('password')}}" placeholder="Enter new Password">
-        </div>
-        @error('password')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-        <div class="form-group">
-            <label for="password_confirmation">Confirm New Password</label>
-            <input type="password" name="password_confirmation" class="form-control" id="password_confirmation"
-                value="{{old('password_confirmation')}}" placeholder="Enter confirm new Password">
-        </div>
-        @error('password_confirmation')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-        @endif
 
-        @if(empty($userRole) )
-        <div class="form-group">
-            <a class="btn btn-primary  mb-3 mt-3" data-toggle="collapse" href="#collapseExample" role="button"
-                aria-expanded="false" aria-controls="collapseExample">
-                Role
-            </a>
-            <div class="collapse" id="collapseExample">
-                <div class="card card-body">
-                    @foreach($roles as $role =>$val)
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="roles[]" id="inlineRadio1"
-                            value="{{$role}}" }}>
-                        <label class="form-check-label" for="inlineRadio1">{{$val}}</label>
-                    </div>
-                    @endforeach
-                </div>
-            </div>
-            <br />
-            @else
-            <a class="btn btn-primary mb-3 mt-3" data-toggle="collapse" href="#collapseExample" role="button"
-                aria-expanded="false" aria-controls="collapseExample">
-                Role
-            </a>
-            <div class="collapse" id="collapseExample">
-                <div class="card card-body">
-                    @foreach($roles as $role)
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" name="roles[]" id="inlineRadio"
-                            value="{{ $role }}" {{ in_array( $role, $userRole ) ? 'checked' : '' }}>
-                        <label class="ford-check-label" for="inlineRadio"> {{ $role}} </label>
-                    </div>
-                    @endforeach
-                </div>
-            </div>
-            <br />
-            @endif
-            @error('roles')
-            <div class="text-danger"> {{$message}} </div>
-            @enderror
-
-            <button type="submit" class="btn btn-primary ">Submit</button>
-            </form>
             {{-- {{ json_encode($errors->all()) }} --}}
             {{-- Update Point --}}
             @if(!empty($user->id))
-
             @foreach ($errors->all() as $error)
             <div class="text-danger">{{ $error }}</div>
             @endforeach
-
             @endif
             <div class="row">
                 @if(!empty($user->id))
@@ -329,9 +167,170 @@
                 </div>
             </div>
             {{-- endnew --}}
-
             @endif
         </div>
+        <div>
+            <h2>EDIT INFORMATION</h2>
+        </div>
+        <form action="{{ route('users.update',[$user->id]) }}" enctype="multipart/form-data" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="hidden" name="id" value="{{$user->id}}">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror"
+                    value="{{ old('name',$user->name) }}" id="name" name="name" placeholder="Enter name">
+                @error('name')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="dateofbirth">Birthday</label>
+                <input type="date" class="form-control birthday" value="{{ old('birthday',$user->birthday) }}"
+                    id="dateofbirth" name="birthday">
+                @error('birthday')
+                <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="phone_number">Phone number</label>
+                <input type="text" class="form-control" value="{{ old('phone_number',$user->phone_number) }}"
+                    id="phone_number" name="phone_number">
+                @error('phone_number')
+                <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control @error('email') is-invalid @enderror"
+                    value="{{ old('email',$user->email) }}" id="email" name="email">
+                @error('email')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="faculty_id">Faculty name</label>
+                <select class="form-control" name="faculty_id" id="faculty_id">
+                    @foreach($faculties as $faculty)
+                    <option value="{{ $faculty->id }}" {{ $user->faculty_id == $faculty->id ? 'selected' : '' }}>
+                        {{ $faculty->name }}
+                    </option>
+                    @endforeach
+                </select>
+                @error('faculty_id')
+                <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            @if($user->gender==null)
+            <div class="form-group">
+                <label for="gender">Gender : </label>
+                <input type="radio" name="gender" value="1">Female
+                <input type="radio" name="gender" value="2">Male
+                @error('gender')
+                <p class="text-danger">{{$message}}</p>
+                @enderror
+            </div>
+            @else
+            <div class="form-group">
+                <label for="gender">Gender : </label>
+                @if($user->gender==1)
+                <label class="checkbox-inline"><input type="radio" name="gender" checked value="1">Female</label>
+                <label class="checkbox-inline"><input type="radio" name="gender" value="2">Male</label>
+                @endif
+                @if($user->gender==2)
+                <label class="checkbox-inline"><input type="radio" name="gender" value="1">Female</label>
+                <label class="checkbox-inline"><input type="radio" name="gender" checked value="2">Male</label>
+                @endif
+                @error('gender')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            @endif
+            <div class="form-group">
+                <label for="avatar">Avatar : </label>
+                <input type="file" id="avatar" class=" form-control @error('avatar') is-invalid @enderror"
+                    name="avatar">
+                @if(!empty($user->avatar))
+                <img src="{{asset ( $user->avatar ) }}" class="rounded-lg ml-2" alt="avatar" width="100px"
+                    height="100px">
+                @endif
+            </div>
+            @error('avatar')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+            @if(!empty( $user->id ))
+            @if((!empty ( $user->password ) && Auth::user()->hasRole('user')) || (
+            Auth::user()->hasRole('admin') &&
+            Auth::user()->id == $user->id && !empty ( Auth::user()->password )))
+            <div class="form-group">
+                <label for="current_password">Old Password</label>
+                <input type="password" name="current_password" class="form-control" id="current_password"
+                    value="{{old('current_password')}}" placeholder="Enter your Password">
+            </div>
+            @endif
+            @error('current_password')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="password">New Password</label>
+                <input type="password" name="password" class="form-control" id="password_confirmation"
+                    value="{{old('password')}}" placeholder="Enter new Password">
+            </div>
+            @error('password')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+            <div class="form-group">
+                <label for="password_confirmation">Confirm New Password</label>
+                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation"
+                    value="{{old('password_confirmation')}}" placeholder="Enter confirm new Password">
+            </div>
+            @error('password_confirmation')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+            @endif
+
+            @if(empty($userRole) )
+            <div class="form-group">
+                <a class="btn btn-primary  mb-3 mt-3" data-toggle="collapse" href="#collapseExample" role="button"
+                    aria-expanded="false" aria-controls="collapseExample">
+                    Role
+                </a>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body">
+                        @foreach($roles as $role =>$val)
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="roles[]" id="inlineRadio1"
+                                value="{{$role}}" }}>
+                            <label class="form-check-label" for="inlineRadio1">{{$val}}</label>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+                <br />
+                @else
+                <a class="btn btn-primary mb-3 mt-3" data-toggle="collapse" href="#collapseExample" role="button"
+                    aria-expanded="false" aria-controls="collapseExample">
+                    Role
+                </a>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body">
+                        @foreach($roles as $role)
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="roles[]" id="inlineRadio"
+                                value="{{ $role }}" {{ in_array( $role, $userRole ) ? 'checked' : '' }}>
+                            <label class="ford-check-label" for="inlineRadio"> {{ $role}} </label>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+                <br />
+                @endif
+                @error('roles')
+                <div class="text-danger"> {{$message}} </div>
+                @enderror
+
+                <button type="submit" class="btn btn-primary ">Submit</button>
+        </form>
     </div>
     <!-- /.content -->
 </div>
